16e279681e2dd0e824bd1b95406217a787684250
mb404778 <166207939+mb404778@users.noreply.github.com>
2024-04-27 16:36:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Start main section
+var counter = 0;
+function increment(){
+    counter++;
+    document.getElementById("Elshenawy").innerHTML = counter;
+}
+
+var counter = 0;
+function Decrement(){
+    counter--;
+    document.getElementById("Elshenawy").innerHTML = counter;
+}
+// End main section
